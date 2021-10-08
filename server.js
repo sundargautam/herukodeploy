6056934178cc14f33a0706ba85919044fe4f4d35
Sundar Gautam <sundargautam6@gmail.com>
2021-10-08 21:23:43 +0545

    added new route

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const dotenv = require("dotenv").config();
 app.get("/",(req,res)=>{
     res.send("<h1>Working</h1>")
 })
+app.get("/mantra",(req,res)=>{
+    res.json({
+        msg:"never give up"
+    })
+})
 const port = process.env.PORT || 8080; 
 app.listen(port,()=>{
     console.log(`listeing at ${port} `);
